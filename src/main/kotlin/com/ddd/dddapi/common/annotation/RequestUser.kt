ca15cfe375de6e0fce13c5ac2678281144c93888
RokwonK <wonrok97@naver.com>
2025-01-17 00:55:56 +0900

    🎨 필요없는 의존성 삭제

--- src/main/kotlin/com/ddd/dddapi/common/annotation/RequestUser.kt
+++ src/main/kotlin/com/ddd/dddapi/common/annotation/RequestUser.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
 @Schema(hidden = true)
 annotation class RequestUser(
     @Schema(hidden = true)
