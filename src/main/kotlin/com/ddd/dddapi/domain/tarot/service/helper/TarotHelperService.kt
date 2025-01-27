150478715c13819f914f5ab6819fe2996b64f5bd
RokwonK <wonrok97@naver.com>
2025-01-28 00:03:28 +0900

    🎨 Layer 수정(HelperService 추가) & 데이터클래스 변환 로직 캡슐화

---
+++ src/main/kotlin/com/ddd/dddapi/domain/tarot/service/helper/TarotHelperService.kt
@@ -0,0 +1,22 @@
+package com.ddd.dddapi.domain.tarot.service.helper
+
+import com.ddd.dddapi.common.exception.BadRequestBizException
+import com.ddd.dddapi.domain.common.annotation.HelperService
+import com.ddd.dddapi.domain.tarot.entity.TarotResultEntity
+import com.ddd.dddapi.domain.tarot.repository.TarotQuestionRepository
+import com.ddd.dddapi.domain.tarot.repository.TarotResultRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.transaction.annotation.Transactional
+
+@HelperService
+@Transactional(readOnly = true)
+class TarotHelperService(
+    private val tarotQuestionRepository: TarotQuestionRepository,
+    private val tarotResultRepository: TarotResultRepository
+) {
+    fun getTarotResultOrThrow(tarotResultId: Long): TarotResultEntity {
+        // TODO: 예외 구체화
+        return tarotResultRepository.findByIdOrNull(tarotResultId)
+            ?: throw BadRequestBizException("타로 결과를 찾을 수 없습니다.")
+    }
+}
