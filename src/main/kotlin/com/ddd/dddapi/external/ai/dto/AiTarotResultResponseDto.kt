5351d2dbfddca5dd553640c92957771aca45073b
RokwonK <wonrok97@naver.com>
2025-01-28 22:07:03 +0900

    🎨 JSON 응답 키 케이스 변환로직 추가(스네이크 -> 카멜케이스)

--- src/main/kotlin/com/ddd/dddapi/external/ai/dto/AiTarotResultResponseDto.kt
+++ src/main/kotlin/com/ddd/dddapi/external/ai/dto/AiTarotResultResponseDto.kt
@@ -3,7 +3,7 @@ package com.ddd.dddapi.external.ai.dto
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy::class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class AiTarotResultResponseDto(
     val type: String,
     val summaryOfDescriptionOfCard: String,
