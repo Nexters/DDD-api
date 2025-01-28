c72f4e4d510cfe77fb36e6ae6250e5eb194c8b95
RokwonK <wonrok97@naver.com>
2025-01-28 16:28:20 +0900

    ✨ 요청메세지용 내부 dto 추가

--- src/main/kotlin/com/ddd/dddapi/domain/chat/dto/InferredInquiryChatMessage.kt
+++ src/main/kotlin/com/ddd/dddapi/domain/chat/dto/InferredInquiryChatMessage.kt
@@ -1,5 +1,11 @@
 package com.ddd.dddapi.domain.chat.dto
 
+import com.ddd.dddapi.common.enums.MessageSender
+import com.ddd.dddapi.common.enums.MessageType
+
 data class InferredInquiryChatMessage(
-    val message: String
+    val messageType: MessageType,
+    val senderType: MessageSender = MessageSender.USER,
+    val message: String,
+    val referenceQuestionId: Long?
 )
