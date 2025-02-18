de7e01fefd1f20a1592fae55ab97c9596a32dfcf
RokwonK <wonrok97@naver.com>
2025-02-19 00:06:00 +0900

    ✨ 트랜잭션 after commit 이벤트 수정

--- src/main/kotlin/com/ddd/dddapi/domain/tarot/service/TarotHistoryServiceImpl.kt
+++ src/main/kotlin/com/ddd/dddapi/domain/tarot/service/TarotHistoryServiceImpl.kt
@@ -35,7 +35,7 @@ class TarotHistoryServiceImpl(
 
     @Async(TAROT_HISTORY_EXECUTOR_NAME)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     override fun saveTarotHistory(request: TarotHistoryEventDto) {
         val user = userHelperService.getUserOrThrow(request.userKey)
         val tarotResult = tarotHelperService.getTarotResultOrThrow(request.tarotResultId)
