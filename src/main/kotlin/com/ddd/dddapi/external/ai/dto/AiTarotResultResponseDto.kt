0d977cac7ad001423de3b86a47ff901118254fab
RokwonK <wonrok97@naver.com>
2025-02-23 15:56:21 +0900

    ✨ 타로결과 추론 시 - 섹션별 요약 기본값 추가

--- src/main/kotlin/com/ddd/dddapi/external/ai/dto/AiTarotResultResponseDto.kt
+++ src/main/kotlin/com/ddd/dddapi/external/ai/dto/AiTarotResultResponseDto.kt
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class AiTarotResultResponseDto(
     val type: String,
-    val summaryOfDescriptionOfCard: String,
+    val summaryOfDescriptionOfCard: String = "카드의 의미",
     val descriptionOfCard: String,
-    val summaryOfAnalysis: String,
+    val summaryOfAnalysis: String = "카드의 답변",
     val analysis: String,
-    val summaryOfAdvice: String,
+    val summaryOfAdvice: String = "타로냥의 조언",
     val advice: String,
     val comprehensiveSummary: String
 )
